efe052b29eab2167623ad11527f870be5204d52e
Aswin_Kumar <aswinkumar1027@gmail.com>
2020-08-05 13:16:01 +0530

    Add files via upload
    first commit _ Base code

---
+++ main.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request
+import datetime
+import gpiozero
+
+app = Flask(__name__)
+robot = gpiozero.Robot(left=(26,16), right=(5,6))
+
+leftend = gpiozero.DigitalInputDevice(17)
+center = gpiozero.DigitalInputDevice(27)
+rightend = gpiozero.DigitalInputDevice(22)
+backl = gpiozero.DigitalInputDevice(23)
+rightl = gpiozero.DigitalInputDevice(19)
+
+Thermal = gpiozero.LED(20)
+pressures = gpiozero.LED(21)
+
+
+def examine():
+    pass
+
+def follow_line():
+    global mode
+    while True:
+        if (center.is_active == True) and (leftend.is_active == False) and (rightend.is_active == False):
+            print("forward")
+            robot.forward()
+       
+        elif (leftend.is_active == False) and (rightend.is_active == True):
+            robot.right()
+            print("right")
+        elif (leftend.is_active == True) and (rightend.is_active == False):
+            robot.left()
+            print("left")
+        
+
+def pressure():
+    pressures.on()
+    
+def Temperature():
+    Thermal.on()
+    
+
+robo_actions = {
+    "forward": robot.forward,
+    "backward": robot.backward,
+    "left": robot.left,
+    "right": robot.right,
+    "stop": robot.stop,
+    "line": follow_line,
+    "examine": examine,
+    "pressure": pressure,
+    "Temperature": Temperature
+}
+
+@app.route("/")                   
+def hello():                      
+    return render_template('main.html')
+
+@app.route("/move", methods=['GET', 'POST'])                   
+def move():
+    movement = request.form['movement']
+    robo_actions[movement]()
+    return "Moving " + movement
+    
+if __name__ == "__main__":        
+    app.run(host='0.0.0.0', debug=True)
